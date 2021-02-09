96691b769ff4744d5140ef00887b448e4fb9a6ac
Abhishek26-eddy <76390205+Abhishek26-eddy@users.noreply.github.com>
2021-02-09 13:14:17 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ssh root@104.236.57.112 <<EOF
+ssh root@18.224.33.239 <<EOF
   cd djtrump
   git pull
   source /opt/envs/djtrump/bin/activate
-  pip install -r requirements.txt
+  pip3 install -r requirements.txt
   ./manage.py migrate
   sudo supervisorctl restart djtrump
   exit
