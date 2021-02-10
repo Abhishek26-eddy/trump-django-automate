740b25d81ea96d013acded08b9d5f75a542ba7b7
Abhishek26-eddy <76390205+Abhishek26-eddy@users.noreply.github.com>
2021-02-10 11:52:24 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -3,7 +3,7 @@
 ssh root@18.224.33.239 <<EOF
   cd trump-django-automate
   git pull
-  source /opt/envs/trump-django-automate/bin/activate
+  source /envs/bin/activate
   pip3 install -r requirements.txt
   ./manage.py migrate
   exit
