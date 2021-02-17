127ceaf9186b5f633a8e69712b3caa6ef676cbe0
Abhishek26-eddy <76390205+Abhishek26-eddy@users.noreply.github.com>
2021-02-17 23:28:42 +0530

    Update deploy_prod.sh

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -6,6 +6,6 @@ ssh root@3.141.38.228 <<EOF
   source /root/djtrump/xyz/bin/activate
   pip3 install -r requirements.txt
   ./manage.py migrate
-  sudo supervisorctl restart trump-django-automate
+  sudo supervisorctl restart djtrump
   exit
 EOF
